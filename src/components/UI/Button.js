2a9212cee313b270894f98d2ed004140b2372d4a
Sunil Kumar R <hisunilkumarr@gmail.com>
2019-11-11 19:34:29 +0530

    "UI Button Fixes!"

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -3,12 +3,12 @@ import Styles from "../../assets/styles/input/c_button.module.scss";
 
 const Button = props => {
   const { classes, name } = props;
-  let newClasses = String(classes);
-  console.log("Class:", newClasses);
-  console.log("Class:", Styles.newClasses);
 
   return (
-    <button className={[Styles.button, Styles.green].join(" ")} type="button">
+    <button
+      className={[Styles.button, Styles[classes]].join(" ")}
+      type="button"
+    >
       {name}
     </button>
   );
